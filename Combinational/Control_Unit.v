ab8502bd84c20a973e3731311f10c20150961434
chintu890 <chintu890@gmail.com>
2014-12-06 02:50:45 -0600

    Fixed ALUCntrl inconsistencies

--- Combinational/Control_Unit.v
+++ Combinational/Control_Unit.v
@@ -61,7 +61,7 @@ module Control_Unit(
     input [5:0] op_in, func_in;
 
     wire [3:0] ALUCntrl_out;
-    wire branch_out, jump_out;s
+    wire branch_out, jump_out;
     wire regWrite_out, regDst_out; 
     wire ALUSrc_out;
     wire memWrite_out, memRead_out, memToReg_out;
@@ -73,20 +73,19 @@ module Control_Unit(
     ADD         = 6'b100_000,
     SUB         = 6'b100_010,
     OR          = 6'b100_101,
-    SLT         = 6'b100_010,
+    SLT         = 6'b101_010,
     AND         = 6'b100_100,
     ADDI        = 6'b001_000,
     LW          = 6'b100_011,
     SW          = 6'b101_011,
     BEQ         = 6'b000_100,
     J           = 6'b000_010,
-    DONTCARE    = 6'bxxx_xxx,
     ZERO        = 6'b000_000;
 
     assign regWrite_out =   (op_in == ADDI) ||
                             (op_in == LW)   ||
                             (op_in == ZERO);
-                            
+
     assign regDst_out   =   op_in == ZERO;
 
     assign ALUSrc_out   =   (op_in == ADDI) ||
@@ -103,12 +102,10 @@ module Control_Unit(
 
     assign ALUCntrl_out =   (op_in == ZERO && func_in == SUB)       ? 4'b0001 :
                             (op_in == ZERO && func_in == AND)       ? 4'b0010 :
-                            (op_in == ZERO && func_in == SLT)       ? 4'b0100 :
                             (op_in == ZERO && func_in == OR)        ? 4'b0101 :
                             (op_in == ZERO && func_in == SLT)       ? 4'b0100 :
-                            (op_in == LW   && func_in == DONTCARE)  ? 4'b1000 :
-                            (op_in == SW   && func_in == DONTCARE)  ? 4'b1000 :
-                            (op_in == BEQ  && func_in == DONTCARE)  ? 4'b1000 : 1'b0;
+                           ((op_in == LW ||
+                             op_in == SW || op_in == BEQ))          ? 4'b1000 : 4'b0000;
 
     assign jump_out     =   op_in == J;
     
@@ -124,7 +121,7 @@ module Control_Unit(
     assign memRead      =   (op_in == ZERO && func_in == ZERO) ? 1'b0   : memRead_out;
     assign jump         =   (op_in == ZERO && func_in == ZERO) ? 1'b0   : jump_out;
     assign ALUCntrl     =   (op_in == ZERO && func_in == ZERO) ? 4'b1111: ALUCntrl_out;
-    
+
 endmodule 
 
 
